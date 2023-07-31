7161f39a444dfe287f554f926ae1d6cee89dc54b
lallemand-fred <114465446+lallemand-fred@users.noreply.github.com>
2023-07-31 11:33:33 +0200

    Ajout de la page animation card

--- JS/appCardAnim.js
+++ JS/appCardAnim.js
@@ -1,9 +1,30 @@
-//movement
+//Mouvements animation
 const card = document.querySelector(".card");
 const container = document.querySelector(".container");
+//Items
+const title = document.querySelector(".title");
+const planete = document.querySelector(".planete img");
+const purchase = document.querySelector(".purchase");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
 
 container.addEventListener("mousemove", (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
-  let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
-  card.style.transform = `rotateY(${yAxis}deg) rotateX(${xAxis}deg)`;
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+//Anim entrée
+card.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  //Pop out
+  title.style.transform = "translateZ(150px)";
+  planete.style.transform = "translateZ(150px) rotateZ(-45deg) ";
+});
+//Anim sortie
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  //Pop back
+  title.style.transform = "translateZ(0px)";
+  planete.style.transform = "translateZ(0px)";
 });
