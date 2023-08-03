4191e32f54e34bd6fc2fde778f337487330d9588
lallemand-fred <114465446+lallemand-fred@users.noreply.github.com>
2023-08-03 19:29:14 +0200

    Ajout effet card 3d sur les 3 cartes img

--- JS/appNavbar2.js
+++ JS/appNavbar2.js
@@ -74,3 +74,39 @@ window.addEventListener("scroll", () => {
   lastScroll = currentScroll;
 });
 //-------------------------------------------------------------------------------
+//Mouvements animation
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+//Items
+const title = document.querySelector(".title");
+const planete = document.querySelector(".planete img");
+const purchase = document.querySelector(".purchase");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
+
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+//Anim entrée
+card.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  //Pop out
+  title.style.transform = "translateZ(70px)";
+  planete.style.transform = "translateZ(90px) rotateZ(-25deg) ";
+  description.style.transform = "translateZ(40px)";
+  sizes.style.transform = "translateZ(70px)";
+  purchase.style.transform = "translateZ(40px)";
+});
+//Anim sortie
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  //Pop back
+  title.style.transform = "translateZ(0px)";
+  planete.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
+});
