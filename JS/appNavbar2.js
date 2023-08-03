9bfd6c58f2829d7f0460da9b43268b7705dafc82
lallemand-fred <114465446+lallemand-fred@users.noreply.github.com>
2023-08-03 20:03:54 +0200

    Annulation effet 3d

--- JS/appNavbar2.js
+++ JS/appNavbar2.js
@@ -74,39 +74,34 @@ window.addEventListener("scroll", () => {
   lastScroll = currentScroll;
 });
 //-------------------------------------------------------------------------------
-//Mouvements animation
-const card = document.querySelector(".card");
-const container = document.querySelector(".container");
-//Items
-const title = document.querySelector(".title");
-const planete = document.querySelector(".planete img");
-const purchase = document.querySelector(".purchase");
-const description = document.querySelector(".info h3");
-const sizes = document.querySelector(".sizes");
+// //Mouvements animation
+// const card = document.querySelector(".cardre");
+// const container = document.querySelector(".containerMenu");
+// //Items
+// const image = document.querySelector(".imgBx img");
+// const title = document.querySelector(".contenu h2");
+// const description = document.querySelector(".contenu p");
+// const btn = document.querySelector(".contenu a");
 
-container.addEventListener("mousemove", (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-});
-//Anim entrée
-card.addEventListener("mouseenter", (e) => {
-  card.style.transition = "none";
-  //Pop out
-  title.style.transform = "translateZ(70px)";
-  planete.style.transform = "translateZ(90px) rotateZ(-25deg) ";
-  description.style.transform = "translateZ(40px)";
-  sizes.style.transform = "translateZ(70px)";
-  purchase.style.transform = "translateZ(40px)";
-});
-//Anim sortie
-container.addEventListener("mouseleave", (e) => {
-  card.style.transition = "all 0.5s ease";
-  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-  //Pop back
-  title.style.transform = "translateZ(0px)";
-  planete.style.transform = "translateZ(0px) rotateZ(0deg)";
-  description.style.transform = "translateZ(0px)";
-  sizes.style.transform = "translateZ(0px)";
-  purchase.style.transform = "translateZ(0px)";
-});
+// card.addEventListener("mousemove", (e) => {
+//   let xAxis = (window.innerWidth / 2 - e.pageX) / 5;
+//   let yAxis = (window.innerHeight / 2 - e.pageY) / 5;
+//   card.style.transform = `rotateY(${yAxis}deg) rotateX(${xAxis}deg)`;
+// });
+// //Anim entrée
+// card.addEventListener("mouseenter", (e) => {
+//   card.style.transition = "none";
+//   //Pop out
+//   // title.style.transform = "translateZ(70px)";
+//   // image.style.transform = "translateZ(90px)";
+//   // description.style.transform = "translateZ(40px)";
+// });
+// //Anim sortie
+// card.addEventListener("mouseleave", (e) => {
+//   card.style.transition = "all 0.5s ease";
+//   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+//   //Pop back
+//   // title.style.transform = "translateZ(0px)";
+//   // image.style.transform = "translateZ(0px) rotateZ(0deg)";
+//   // description.style.transform = "translateZ(0px)";
+// });
