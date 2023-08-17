2e4ef28412899e44ccbe3150ebc2c508f43fea62
lallemand-fred <114465446+lallemand-fred@users.noreply.github.com>
2023-08-17 23:20:27 +0200

    Modiff fichier js effet pluies de mots

--- JS/appPluisDeMots.js
+++ JS/appPluisDeMots.js
@@ -11,4 +11,17 @@ document.onmousemove = function (e) {
   let element = document.createElement("div");
   element.className = "element";
   body.prepend(element);
+  //mouvement aléatoire sur l'axe y & x
+  element.style.left = cursor.getBoundingClientRect().x + "px";
+  element.style.top = cursor.getBoundingClientRect().y + "px";
+  setTimeout(function () {
+    let text = document.querySelectorAll(".element")[0],
+      directionX = Math.random() < 0.5 ? -1 : 1,
+      directionY = Math.random() < 0.5 ? -1 : 1;
+
+    text.style.left =
+      parseInt(text.style.left) - directionX * (Math.random() * 200) + "px";
+    text.style.top =
+      parseInt(text.style.top) - directionY * (Math.random() * 200) + "px";
+  });
 };
