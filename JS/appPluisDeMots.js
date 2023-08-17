a9a3119945b003fc790bdd51cec3ef76a7dca849
lallemand-fred <114465446+lallemand-fred@users.noreply.github.com>
2023-08-18 00:29:16 +0200

    Modiff de l'effet pluie de lettres

--- JS/appPluisDeMots.js
+++ JS/appPluisDeMots.js
@@ -4,16 +4,20 @@ document.onmousemove = function (e) {
   //mouvements curseur
   cursor.style.top = e.pageY + "px";
   cursor.style.left = e.pageX + "px";
-  //animation arrière curseur
+
+  //animation background curseur
   body.style.backgroundPositionX = e.pageX / -4 + "px";
   body.style.backgroundPositionY = e.pageY / -4 + "px";
+
   //Ajout des éléments au corp de page
   let element = document.createElement("div");
   element.className = "element";
   body.prepend(element);
+
   //mouvement aléatoire sur l'axe y & x
   element.style.left = cursor.getBoundingClientRect().x + "px";
   element.style.top = cursor.getBoundingClientRect().y + "px";
+
   setTimeout(function () {
     let text = document.querySelectorAll(".element")[0],
       directionX = Math.random() < 0.5 ? -1 : 1,
@@ -25,5 +29,11 @@ document.onmousemove = function (e) {
       parseInt(text.style.top) - directionY * (Math.random() * 200) + "px";
     text.style.opacity = 0;
     text.style.transform = "scale(0.25)";
-  });
+    text.innerHTML = "hi";
+
+    //suppresion des éléments
+    setTimeout(function () {
+      element.remove();
+    }, 1000);
+  }, 10);
 };
