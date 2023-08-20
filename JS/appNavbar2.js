8b25ea65802e951b89647fe3d0c19d3adc791d43
lallemand-fred <114465446+lallemand-fred@users.noreply.github.com>
2023-08-20 13:57:09 +0200

    Ajout et modiff style sous ligne logo

--- JS/appNavbar2.js
+++ JS/appNavbar2.js
@@ -51,28 +51,28 @@ window.addEventListener("scroll", function () {
 
 //-------------------Scroll up apparition des menus, scroll bas disparition----------------
 
-const body = document.body;
-let lastScroll = 0;
+// const body = document.body;
+// let lastScroll = 0;
 
-window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
+// window.addEventListener("scroll", () => {
+//   const currentScroll = window.pageYOffset;
 
-  if (currentScroll <= 0) {
-    body.classList.remove("scroll-up");
-  }
+//   if (currentScroll <= 0) {
+//     body.classList.remove("scroll-up");
+//   }
 
-  if (currentScroll < lastScroll && !body.classList.contains("scroll-down")) {
-    body.classList.remove("scroll-up");
-    body.classList.add("scroll-down");
-  }
+//   if (currentScroll < lastScroll && !body.classList.contains("scroll-down")) {
+//     body.classList.remove("scroll-up");
+//     body.classList.add("scroll-down");
+//   }
 
-  if (currentScroll > lastScroll && body.classList.contains("scroll-down")) {
-    body.classList.remove("scroll-down");
-    body.classList.add("scroll-up");
-  }
+//   if (currentScroll > lastScroll && body.classList.contains("scroll-down")) {
+//     body.classList.remove("scroll-down");
+//     body.classList.add("scroll-up");
+//   }
 
-  lastScroll = currentScroll;
-});
+//   lastScroll = currentScroll;
+// });
 // ---------------------------Hover wave links work--------------------------
 let text = document.querySelectorAll("ul li a").forEach((text) => {
   text.innerHTML = text.innerText
