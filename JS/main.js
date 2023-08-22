f976ab4c2970aaa35e11e0e77fa58643128b3397
lallemand-fred <114465446+lallemand-fred@users.noreply.github.com>
2023-08-22 17:59:33 +0200

    Modiff déplacement code js

--- JS/main.js
+++ JS/main.js
@@ -9,11 +9,11 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 
-// ----------------------------------Carousel-------------------------------
 const slideElements = document.querySelectorAll(
   ".slidG, .slidD, .slidB, .slidH, .slidO"
 );
 slideElements.forEach((el) => observer.observe(el));
+// ----------------------------------Carousel-------------------------------
 
 /**
  * Permet de rajouter la navigation tactile pour le carousel
